be814f18258d6dba1646b818e8ebcc3ed256ab15
Flávia Torres <flaviiadev@gmail.com>
2023-06-23 22:06:03 -0300

    Implementando o metódo de cadastrar produto

--- src/main/java/br/com/api/produtos/controle/produtoControle.java
+++ src/main/java/br/com/api/produtos/controle/produtoControle.java
@@ -1,12 +1,24 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.modelo.produtoModelo;
+import br.com.api.produtos.servico.produtoServico;
 
 @RestController
 public class produtoControle {
 
-    @GetMapping
+    @Autowired
+    private produtoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<produtoModelo> listar(){
+        return ps.listar();
+    }
+
+    @GetMapping("/")
     public String rota(){
         return "App funcionando";
     }
--- src/main/java/br/com/api/produtos/servico/produtoServico.java
+++ src/main/java/br/com/api/produtos/servico/produtoServico.java
@@ -1,5 +1,40 @@
 package br.com.api.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.modelo.produtoModelo;
+import br.com.api.produtos.modelo.respostaModelo;
+import br.com.api.produtos.repositorio.produtoRepositorio;
+
+@Service
 public class produtoServico {
     
+    @Autowired
+    private produtoRepositorio pr;
+
+    @Autowired
+    private respostaModelo rm;
+
+    //Retornar todos os produtos
+    public Iterable<produtoModelo> listar(){
+        return pr.findAll();
+    }
+
+    //Metódo para cadastrar produtos
+    public ResponseEntity<?> cadastrar(produtoModelo pm){
+
+        if(pm.getNome().equals("")){
+            rm.setMensagem("O nome do produto é obrigatório!");
+            return new ResponseEntity<respostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setMensagem("O nome da marca é obrigatório!");
+            return new ResponseEntity<respostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<produtoModelo>(pr.save(pm), HttpStatus.CREATED);
+        }
+        
+    }
 }
