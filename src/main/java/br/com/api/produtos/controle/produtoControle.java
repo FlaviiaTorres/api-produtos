93b8e5a2ef52744aa55a9748de47c0ed67a131d9
Flávia Torres <flaviiadev@gmail.com>
2023-06-23 21:46:06 -0300

    Ajustando a classe de mensagem de erro

--- src/main/java/br/com/api/produtos/controle/produtoControle.java
+++ src/main/java/br/com/api/produtos/controle/produtoControle.java
@@ -1,5 +1,14 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class produtoControle {
+
+    @GetMapping
+    public String rota(){
+        return "App funcionando";
+    }
     
 }
