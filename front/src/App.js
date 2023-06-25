7e1d490c96fdd8ec353468f7bdbfed4a398437d6
Flávia Torres <flaviiadev@gmail.com>
2023-06-25 12:38:07 -0300

    Obtendo Produtos

--- front/src/App.js
+++ front/src/App.js
@@ -7,9 +7,18 @@ import React, { useEffect, useState } from "react";
 function App() {
 
 const [btnCadastrar, setbtnCadastrar] = useState(true);
+const [produtos, setProdutos] = useState([]);
+
+//UseEffect
+useEffect(()=>{
+  fetch("http://localhost:8080/listar")
+  .then(retorno => retorno.json())
+  .then(retorno_convertido => setProdutos(retorno_convertido));
+}, []);
 
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar} />
       <Tabela />
     </div>
